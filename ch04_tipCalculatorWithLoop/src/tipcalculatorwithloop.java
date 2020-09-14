d567fb97e78628e6fe602cf5bbc887d691f1e8a3
philip loeffler <philiploeffler@gmail.com>
2020-09-14 18:37:29 -0400

    interest with loop

---
+++ ch04_tipCalculatorWithLoop/src/tipcalculatorwithloop.java
@@ -0,0 +1,38 @@
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class tipcalculatorwithloop {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Tip calculator\n");
+
+		NumberFormat currency = NumberFormat.getCurrencyInstance();
+		NumberFormat percent = NumberFormat.getPercentInstance();
+		String choice;
+		do {
+			System.out.println("cost of meal:");
+			BigDecimal cost = sc.nextBigDecimal();
+			BigDecimal tipIncrement = new BigDecimal("0.05");
+			BigDecimal tipPercent = new BigDecimal("0.15");
+
+			for (int i = 0; i < 3; ++i) {
+				// calculate tip and total
+				BigDecimal tipAmount = cost.multiply(tipPercent);
+				BigDecimal totalCost = cost.add(tipAmount);
+
+				System.out.print(percent.format(tipPercent));
+				System.out.print("tip amount" + currency.format(tipAmount));
+				System.out.print("total amount" + currency.format(totalCost));
+
+				// increment to next tip amount
+				tipPercent = tipPercent.add(tipIncrement);
+			}
+			System.out.println("Continue? (y/n)");
+			choice = sc.next();
+		} while (choice.equalsIgnoreCase("y"));
+
+	}
+
+}
